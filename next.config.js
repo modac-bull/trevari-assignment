09ad082a9e5d4699c243266155c32b181a4fbfe8
leejihoon <ljhljh211@naver.com>
2024-01-02 22:13:15 +0900

    style: BookItem 컴포넌트 스타일 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withTwin = require('./withTwin.js')
  * @type {import('next').NextConfig}
  */
 module.exports = withTwin({
+  images: {
+    domains: ['itbook.store'],
+  },
   reactStrictMode: true,
   experimental: {
     serverActions: true,
--- src/components/pages/home/BookItem.tsx
+++ src/components/pages/home/BookItem.tsx
@@ -1,5 +1,7 @@
 import { BookType } from '@/apis/search/search.type'
+import Link from 'next/link'
 import { styled, theme } from 'twin.macro'
+import Image from 'next/image'
 
 type Props = {
   data: BookType
@@ -8,14 +10,21 @@ type Props = {
 export default function BookItem({ data }: Props) {
   const title = data.title
   const subtitle = data.subtitle
-  const image = data.image
   const url = data.url
   return (
     <BookItemContainer>
-      <div>{title}</div>
-      <div>{subtitle}</div>
-      <div>{image}</div>
-      <div>{url}</div>
+      <Link href={`/book/${data.isbn13}`}>
+        <Image
+          src={data.image}
+          width={50}
+          height={50}
+          alt={data.title}
+          style={{ width: 'auto' }}
+        />
+        <Title>{title}</Title>
+        <Subtitle>{subtitle}</Subtitle>
+        <UrlText>{url}</UrlText>
+      </Link>
     </BookItemContainer>
   )
 }
@@ -23,4 +32,22 @@ export default function BookItem({ data }: Props) {
 const BookItemContainer = styled.div`
   padding: 10px 20px;
   border-radius: 8px;
+  border: 1px solid ${theme`colors.gray.300`};
+  margin-bottom: 10px;
+`
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`
+const Subtitle = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  margin-bottom: 10px;
+`
+const UrlText = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  margin-bottom: 10px;
 `
